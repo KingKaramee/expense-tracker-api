8464541f688c19d5299b7fbe9f1c3e612c5923e4
big <big@localhost.localdomain>
2020-12-31 19:24:59 +0100

    Added Model for Expense and Test for it

--- restapi/.ipynb_checkpoints/tests-checkpoint.py
+++ restapi/.ipynb_checkpoints/tests-checkpoint.py
@@ -1,3 +1,19 @@
+## TEST DDT
 from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
+class Testmodels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=100,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(100, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,12 +1,19 @@
 ## TEST DDT
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
-def two_interger_sum(a, b):
-    return a + b
+class Testmodels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=100,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
 
-
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_interger_sum(1, 2), 3)
+        self.assertEqual(100, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
